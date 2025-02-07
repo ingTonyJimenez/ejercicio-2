8652e8743f552e9cab0ce33792d916f7849ae474
Tony Jimenez <ingtonyjimenez@gmail.com>
2025-02-07 14:35:11 -0600

    Se amplía la lógica de los intentos

--- app.js
+++ app.js
@@ -1,12 +1,49 @@
+let intentos = 1;
+let numeroSecreto = generarNumeroSecreto();
+console.log(numeroSecreto);
+
+// Asignación de funciones
+
+
 
 
 function asignarTextoElemento(elemento, texto) {
   let elementoHTML = document.querySelector(elemento);
   elementoHTML.innerHTML = texto;
+  return;
 }
-function intentoDeUsuario() {
-  alert("click desde el botón");
+
+
+
+
+function verificarIntento() {
+
+console.log(intentos);
+let numeroDeUsuario = parseInt(document.getElementById("valorUsuario").value);
+
+if(numeroDeUsuario===numeroSecreto){
+asignarTextoElemento("p", `Acertaste en ${intentos}  ${intentos===1 ? "intento" : "intentos"}`);
 }
+
+else{
+    if(numeroDeUsuario>numeroSecreto){
+        asignarTextoElemento("p", "El número secreto es menor");
+    }
+    else{
+        asignarTextoElemento("p", "El número secreto es mayor");
+    }
+    intentos++;
+}
+return;
+}
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10)+1;
+
+}
+
+
+
 
 asignarTextoElemento("h1","Juego del número secreto");
 asignarTextoElemento(".texto__parrafo","Indica un número del 1 al 100");
