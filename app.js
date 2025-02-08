1127e777480e5890d867f3a8a9b803ca56222cc8
Tony Jimenez <ingtonyjimenez@gmail.com>
2025-02-07 22:59:24 -0600

    Incluimos funciones de nuevo juego

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 let intentos = 1;
-let numeroSecreto = generarNumeroSecreto();
-console.log(numeroSecreto);
+let numeroSecreto = 0;
+// console.log(numeroSecreto);
 
 // Asignación de funciones
+
 
 
 
@@ -14,8 +15,6 @@ function asignarTextoElemento(elemento, texto) {
 }
 
 
-
-
 function verificarIntento() {
 
 console.log(intentos);
@@ -23,27 +22,61 @@ let numeroDeUsuario = parseInt(document.getElementById("valorUsuario").value);
 
 if(numeroDeUsuario===numeroSecreto){
 asignarTextoElemento("p", `Acertaste en ${intentos}  ${intentos===1 ? "intento" : "intentos"}`);
+document.getElementById("reiniciar").removeAttribute("disabled");
 }
 
 else{
+
+    // El usuario no acertó
+
     if(numeroDeUsuario>numeroSecreto){
         asignarTextoElemento("p", "El número secreto es menor");
     }
     else{
         asignarTextoElemento("p", "El número secreto es mayor");
     }
+    limpiarCajaTexto();
     intentos++;
 }
+
+
 return;
 }
 
 function generarNumeroSecreto() {
     return Math.floor(Math.random()*10)+1;
-
 }
 
+function limpiarCajaTexto(){
+  document.querySelector("#valorUsuario").value = "";
+
+  
+}
+
+// function habilitarBoton(){
+//     document.getElementById("reiniciar").setAttribute("enabled");
+// }
+
+ function deshabilitarBoton(){
+     document.getElementById("reiniciar").setAttribute("disabled", true);
+ }
+
+function condicionesIniciales(){
+    asignarTextoElemento("h1","Juego del número secreto");
+    asignarTextoElemento(".texto__parrafo","Indica un número del 1 al 100"); 
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+}
+
+function reiniciarJuego(){
+// Limpiar la caja
+limpiarCajaTexto();
+// Cargar los mensajes iniciales
+condicionesIniciales();
+//  Deshabilitar el boton
+deshabilitarBoton();
+}
+
+condicionesIniciales();
 
 
-
-asignarTextoElemento("h1","Juego del número secreto");
-asignarTextoElemento(".texto__parrafo","Indica un número del 1 al 100");
